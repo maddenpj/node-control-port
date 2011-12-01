4e95dc29e950da8a6d6f9f5e1736f1e2b1c0463a
Patrick Madden <maddenpj@gmail.com>
2011-12-01 16:10:15 -0600

    Added Colors

--- ControlPort.js
+++ ControlPort.js
@@ -3,7 +3,8 @@ var net = require('net');
 function ControlPort(prompt_) {
 	var p = prompt_ || 'Node';
 	
-	this.prompt = '\033[1;33m'+p+'> \033[22;39m';
+	this.prompt = p+'> \033[22;39m';
+	this.color = 'yellow';
 	this.commands = {};
 	this.menu = '---- Menu ----\n\n';
 }
@@ -13,31 +14,36 @@ ControlPort.prototype.start = function (port) {
 	this.port = port;
 	var self = this;
 	net.createServer(function (socket) {
-		socket.write(self.prompt);
+		socket.write(colorizePrompt(self.prompt,self.color));
 		socket.on('data', function (data) {
 			self.handler(data,socket);
 		});
 	}).listen(this.port);
 	
 	this.register('menu',function() {
-			return this.menu+'\n';
+		return this.menu+'\n';
 	});
 }
 
+function colorizePrompt(prompt,color) {
+	if(Colors[color] === undefined) {
+		color = 'yellow'
+	}
+	return Colors[color] + prompt;
+}
 
 ControlPort.prototype.handler = function (data,socket) { 
 	var parse = data.toString().substring(0, data.length-1).split(/\s+/g);
-	
 	var command = parse.shift();
 	
 	if(parse[0] === '') parse.shift();
 
 	if( this.commands[command] === undefined) { 
-		socket.write('\n'+this.prompt);
+		socket.write('\n'+colorizePrompt(this.prompt,this.color));
 		return;
 	}
 	var out = this.commands[command].apply(this, parse); 
-	socket.write(out+'\n'+this.prompt); 
+	socket.write(out+'\n'+colorizePrompt(this.prompt,this.color));
 }
 
 ControlPort.prototype.addToMenu = function (name, args,descrip) {
@@ -70,6 +76,18 @@ function extractArguments (string) {
 	}
 	return args;
 }
+
+
+var Colors = {
+yellow : '\033[1;33m',
+blue   : '\033[1;34m',
+cyan   : '\033[1;36m',
+green  : '\033[1;32m',
+magenta: '\033[1;35m',
+red    : '\033[1;31m'
+};
+
+
 
 
 exports.ControlPort = ControlPort;
